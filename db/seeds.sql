0746322e9342dd54efd0aa06ca6835a5ce1daff1
swoboda <michal.swoboda@outlook.com>
2019-11-12 17:55:33 -0500

    file setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Meeses');
+INSERT INTO burgers (name) VALUES ('Bongo Cat');
+INSERT INTO burgers (name) VALUES ('Avocado Toast',TRUE);
+INSERT INTO burgers (name) VALUES ('McWhopper');
+INSERT INTO burgers (name) VALUES ('Turkey');
